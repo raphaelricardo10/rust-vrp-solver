c18a7bb4e465429980a504a66e7f5234b1e3d33f
Raphael <raphaelricardo10@gmail.com>
2022-11-02 18:03:52 -0300

    Added the id to stop

--- src/domain/stop.rs
+++ src/domain/stop.rs
@@ -1,9 +1,14 @@
 pub struct Stop {
+    id: u32,
     pub usage: u32,
 }
 
 impl Stop {
-    pub fn new() -> Stop {
-        Stop { usage: 0 }
+    pub fn new(id: u32) -> Stop {
+        Stop { id, usage: 0 }
+    }
+
+    pub fn get_id(&self) -> u32 {
+        self.id
     }
 }
