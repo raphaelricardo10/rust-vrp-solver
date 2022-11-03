95e1dd5185e9310eaa686b4748a23d48d41fdc5f
Raphael <raphaelricardo10@gmail.com>
2022-11-03 13:22:54 -0300

    Feat: created a solver for greedy algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod tests;
 pub mod domain;
 pub mod errors;
+pub mod solvers;
 pub mod services;
---
+++ src/solvers/greedy/solver.rs
@@ -0,0 +1,30 @@
+use crate::{
+    domain::{route::DistanceMatrix, stop::Stop, vehicle::Vehicle},
+    services::route_service::RouteService,
+};
+
+pub fn solve<'a>(vehicle: Vehicle, distances: &'a DistanceMatrix, stops: &'a Vec<Stop>) -> Vec<u32> {
+
+    let vehicles = &mut vec![vehicle];
+    let mut route_service = RouteService::new(vehicles, distances, stops);
+
+    route_service.assign_stop_to_route(0, 0);
+
+    while route_service.get_available_stops().len() != 0 {
+        let current_stop_pos = route_service.get_route(0).get_stops().len() - 1;
+        let current_stop_id = route_service
+            .get_route(0)
+            .get_stops()
+            .get(current_stop_pos)
+            .unwrap()
+            .get_id();
+        let closest_stop = route_service.get_closest_stop(current_stop_id);
+        route_service.assign_stop_to_route(0, closest_stop.get_id());
+    }
+
+    route_service
+        .get_route(0)
+        .get_stops()
+        .iter()
+        .map(|x| x.get_id()).collect()
+}
