cad328bb92fa372bd8159ffa43c73bd57941efa8
Raphael <raphaelricardo10@gmail.com>
2022-11-03 13:23:38 -0300

    Test: added the test for the solution of greedy algorithm

---
+++ src/tests/greedy_solver.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+
+use crate::{
+    domain::{route::DistanceMatrix, stop::Stop, vehicle::Vehicle},
+    solvers::greedy::solver::solve,
+};
+
+#[test]
+fn greedy_solution_is_correct() {
+    let vehicle = Vehicle::new(0, 10);
+
+    let mut stops: Vec<Stop> = Vec::new();
+    stops.push(Stop::new(0, 0));
+    stops.push(Stop::new(1, 0));
+    stops.push(Stop::new(2, 0));
+    stops.push(Stop::new(3, 0));
+
+    let mut distances: DistanceMatrix = HashMap::new();
+    distances.insert((0, 1), 2.0);
+    distances.insert((0, 2), 1.0);
+    distances.insert((0, 3), 3.0);
+
+    distances.insert((1, 0), 2.0);
+    distances.insert((1, 2), 5.0);
+    distances.insert((1, 3), 3.0);
+
+    distances.insert((2, 0), 1.0);
+    distances.insert((2, 1), 5.0);
+    distances.insert((2, 3), 2.0);
+
+    distances.insert((3, 0), 3.0);
+    distances.insert((3, 1), 3.0);
+    distances.insert((3, 2), 2.0);
+
+    let solution = solve(vehicle, &distances, &stops);
+
+    assert_eq!(solution[0], 0);
+    assert_eq!(solution[1], 2);
+    assert_eq!(solution[2], 3);
+    assert_eq!(solution[3], 1);
+}
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -6,3 +6,6 @@ pub mod vehicle_test;
 
 #[cfg(test)]
 pub mod route_service_test;
+
+#[cfg(test)]
+pub mod greedy_solver;
