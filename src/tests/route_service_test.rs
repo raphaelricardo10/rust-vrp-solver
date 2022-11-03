3705e42721b871a72e8961bdee98efa9e7f538df
Raphael <raphaelricardo10@gmail.com>
2022-11-03 13:23:24 -0300

    Test: added test to get the closest stop

--- src/tests/route_service_test.rs
+++ src/tests/route_service_test.rs
@@ -39,3 +39,24 @@ fn can_assign_stop_to_route() {
 
     assert_eq!(route_service.get_route(0).get_stops().len(), 1)
 }
+
+#[test]
+fn can_get_closes_stop() {
+    let mut vehicles: Vec<Vehicle> = Vec::new();
+    vehicles.push(Vehicle::new(0, 5));
+
+    let mut stops: Vec<Stop> = Vec::new();
+    stops.push(Stop::new(0, 0));
+    stops.push(Stop::new(1, 0));
+    stops.push(Stop::new(2, 0));
+    stops.push(Stop::new(3, 0));
+
+    let mut distances: DistanceMatrix = HashMap::new();
+    distances.insert((0, 1), 2.0);
+    distances.insert((0, 2), 1.0);
+    distances.insert((0, 3), 3.0);
+
+    let route_service = RouteService::new(&mut vehicles, &distances, &stops);
+
+    assert_eq!(route_service.get_closest_stop(0).get_id(), 2);
+}
