62ea41f537238ae209d9f371b569ed08b47bf48b
Raphael <raphaelricardo10@gmail.com>
2022-11-02 22:35:27 -0300

    Added some attributes and getters

--- src/services/route_service.rs
+++ src/services/route_service.rs
@@ -1,14 +1,38 @@
-use crate::domain::{route::Route, vehicle::Vehicle};
+use crate::domain::{
+    route::{DistanceMatrix, Route},
+    vehicle::Vehicle, stop::Stop,
+};
 
-pub struct RouteService {
-    routes: Vec<Route>,
+pub struct RouteService<'a> {
+    stops: Vec<Stop>,
+    routes: Vec<Route<'a>>,
+    vehicles: Vec<Vehicle>,
+    distances: DistanceMatrix,
 }
 
-impl RouteService {
-    pub fn init_routes(&mut self, vehicles: Vec<Vehicle>) {
-        for vehicle in vehicles {
-            let route = Route::new(vehicle);
-            self.routes.push(route);
+impl<'a> RouteService<'a> {
+    pub fn new(&'a mut self, vehicles: Vec<Vehicle>, distances: DistanceMatrix, stops: Vec<Stop>) {
+        self.stops = stops;
+        self.vehicles = vehicles;
+        self.distances = distances;
+
+        self.routes = Vec::new();
+
+        for vehicle in &mut self.vehicles {
+            self.routes.push(Route::new(vehicle, &self.distances));
         }
     }
+
+    pub fn get_stops(&self) -> &Vec<Stop> {
+        &self.stops
+    }
+
+    pub fn get_routes(&self) -> &Vec<Route> {
+        &self.routes
+    }
+
+    pub fn get_vehicles(&self) -> &Vec<Vehicle> {
+        &self.vehicles
+    }
+
 }
