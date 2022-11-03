59aaab823f7e16098ca105c0425e79f66ec15803
Raphael <raphaelricardo10@gmail.com>
2022-11-03 13:59:57 -0300

    Refactor: removed hardcoded vehicle id

--- src/solvers/greedy/solver.rs
+++ src/solvers/greedy/solver.rs
@@ -8,21 +8,23 @@ pub fn solve<'a>(vehicle: Vehicle, distances: &'a DistanceMatrix, stops: &'a Vec
     let vehicles = &mut vec![vehicle];
     let mut route_service = RouteService::new(vehicles, distances, stops);
 
-    route_service.assign_stop_to_route(0, 0);
+    let vehicle_id = route_service.get_vehicles().first().unwrap().get_id();
+
+    route_service.assign_stop_to_route(vehicle_id, 0);
 
     while !route_service.has_available_stop() {
         let current_stop_id = route_service
-            .get_route(0)
+            .get_route(vehicle_id)
             .get_current_stop()
             .get_id();
 
         let nearest_stop = route_service.get_nearest_stop(current_stop_id);
-        
-        route_service.assign_stop_to_route(0, nearest_stop.get_id());
+
+        route_service.assign_stop_to_route(vehicle_id, nearest_stop.get_id());
     }
 
     route_service
-        .get_route(0)
+        .get_route(vehicle_id)
         .get_stops()
         .iter()
         .map(|x| x.get_id()).collect()
