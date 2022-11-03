c02c1019008680bbece1186d10c488e49d035133
Raphael <raphaelricardo10@gmail.com>
2022-11-03 08:35:52 -0300

    Test: added test to assign a stop to route

--- src/tests/route_service_test.rs
+++ src/tests/route_service_test.rs
@@ -20,3 +20,22 @@ fn route_service_started() {
     assert_eq!(route_service.get_routes().len(), 2);
     assert_eq!(route_service.get_vehicles().len(), 2);
 }
+
+#[test]
+fn can_assign_stop_to_route() {
+    let mut vehicles: Vec<Vehicle> = Vec::new();
+    vehicles.push(Vehicle::new(0, 5));
+    vehicles.push(Vehicle::new(1, 10));
+
+    let mut stops: Vec<Stop> = Vec::new();
+    stops.push(Stop::new(0, 2));
+    stops.push(Stop::new(1, 3));
+
+    let distances: DistanceMatrix = HashMap::new();
+
+    let mut route_service = RouteService::new(&mut vehicles, &distances, &stops);
+
+    route_service.assign_stop_to_route(0, 0);
+
+    assert_eq!(route_service.get_routes().get(&0).unwrap().get_stops().len(), 1)
+}
