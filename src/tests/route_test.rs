ae35b7fb45a07776569af3e3dc2743858c6d9c05
Raphael <raphaelricardo10@gmail.com>
2022-11-06 11:45:20 -0300

    Test: changed to pass the distances manually

--- src/tests/route_test.rs
+++ src/tests/route_test.rs
@@ -12,25 +12,26 @@ use super::fixtures::{distances, full_stops, stops};
 fn route_distance_calculation(stops: Vec<Stop>, distances: DistanceMatrix) {
     let mut vehicle = Vehicle::new(0, 10);
 
-    let mut route = Route::new(&mut vehicle, &distances);
+    let mut route = Route::new(&mut vehicle);
 
-    route.add_stop(&stops[0]).unwrap();
-    route.add_stop(&stops[1]).unwrap();
-    route.add_stop(&stops[2]).unwrap();
-    route.add_stop(&stops[3]).unwrap();
+    route.add_stop(&stops[0], 0.0).unwrap();
+    route.add_stop(&stops[1], *distances.get(&(stops[0].get_id(), stops[1].get_id())).unwrap()).unwrap();
+    route.add_stop(&stops[2], *distances.get(&(stops[1].get_id(), stops[2].get_id())).unwrap()).unwrap();
+    route.add_stop(&stops[3], *distances.get(&(stops[2].get_id(), stops[3].get_id())).unwrap()).unwrap();
 
-    assert_eq!(route.total_distance().unwrap(), 9.0);
+    assert_eq!(route.total_distance(), 9.0);
 }
 
 #[rstest]
 fn route_cannot_overload_vehicle(full_stops: Vec<Stop>, distances: DistanceMatrix) {
     let mut vehicle = Vehicle::new(0, 10);
 
-    let mut route = Route::new(&mut vehicle, &distances);
+    let mut route = Route::new(&mut vehicle);
 
-    route.add_stop(&full_stops[0]).unwrap();
+    route.add_stop(&full_stops[0], 0.0).unwrap();
+    let distance = *distances.get(&(full_stops[0].get_id(), full_stops[1].get_id())).unwrap();
 
-    match route.add_stop(&full_stops[1]) {
+    match route.add_stop(&full_stops[1], distance) {
         Ok(_) => assert!(false),
         Err(_) => assert!(true),
     }
