90a693c7b8598515a7aead8686d2b6476a121915
Raphael <raphaelricardo10@gmail.com>
2022-11-05 22:16:18 -0300

    Refactor: removed the distance matrix abstraction and created the distance_service struct

--- src/services/distance/distance_matrix.rs
+++ src/services/distance/distance_matrix.rs
@@ -1,18 +1,19 @@
-use std::{cmp::Ordering, collections::HashMap};
+use std::cmp::Ordering;
 
 use crate::domain::stop::Stop;
 
-pub type DistanceMatrixKey<T> = (T, T);
-pub type DistanceMatrixMap<K, V> = HashMap<DistanceMatrixKey<K>, V>;
-
-pub (crate) struct DistanceMatrixEntry<'a> {
+pub(crate) struct DistanceMatrixEntry<'a> {
     distance: f64,
     source: &'a Stop,
     destination: &'a Stop,
 }
 
 impl<'a> DistanceMatrixEntry<'a> {
-    pub(crate) fn new(source: &'a Stop, destination: &'a Stop, distance: f64) -> DistanceMatrixEntry {
+    pub(crate) fn new(
+        source: &'a Stop,
+        destination: &'a Stop,
+        distance: f64,
+    ) -> DistanceMatrixEntry {
         DistanceMatrixEntry {
             source,
             distance,
@@ -46,7 +47,3 @@ impl<'a> PartialEq for DistanceMatrixEntry<'a> {
 }
 
 impl<'a> Eq for DistanceMatrixEntry<'a> {}
-
-pub struct DistanceMatrix<K, V> {
-    pub(crate) entries: DistanceMatrixMap<K, V>,
-}
--- src/services/distance/distance_service.rs
+++ src/services/distance/distance_service.rs
@@ -2,34 +2,47 @@ use std::collections::HashMap;
 
 use crate::domain::stop::Stop;
 
-use super::distance_matrix::{DistanceMatrix, DistanceMatrixEntry, DistanceMatrixMap};
+use super::distance_matrix::DistanceMatrixEntry;
 
-impl<'a> DistanceMatrix<u32, &DistanceMatrixEntry<'a>> {
-    pub fn new(
-        entries: DistanceMatrixMap<&'a Stop, f64>,
-    ) -> DistanceMatrix<u32, DistanceMatrixEntry> {
-        let mut distance_matrix: DistanceMatrixMap<u32, DistanceMatrixEntry> = HashMap::new();
+pub type DistancesMatrixKey = (u32, u32);
+pub type DistanceMatrixInput = HashMap<DistancesMatrixKey, f64>;
+pub type DistanceMatrix<'a> = HashMap<DistancesMatrixKey, DistanceMatrixEntry<'a>>;
 
-        for entry in entries {
-            let source_stop: &Stop = entry.0 .0;
-            let destination_stop: &Stop = entry.0 .1;
-            let distance: f64 = entry.1;
+pub type StopsMap<'a> = HashMap<u32, &'a Stop>;
 
-            let stop_distance = DistanceMatrixEntry::new(source_stop, destination_stop, distance);
+pub struct DistanceService<'a> {
+    distances: DistanceMatrix<'a>,
+}
 
-            distance_matrix.insert(
-                (source_stop.get_id(), destination_stop.get_id()),
-                stop_distance,
-            );
+impl<'a> DistanceService<'a> {
+    pub fn new(stops: StopsMap<'a>, distances: &'a DistanceMatrixInput) -> DistanceService<'a> {
+        let mut mapped_distances: DistanceMatrix = HashMap::new();
+
+        for distance in distances {
+            let source_stop_id = distance.0 .0;
+            let destination_stop_id = distance.0 .1;
+            let distance = distance.1;
+
+            let source_stop = *stops.get(&source_stop_id).unwrap();
+            let destination_stop = *stops.get(&source_stop_id).unwrap();
+
+            let stop_distance =
+                DistanceMatrixEntry::new(source_stop, destination_stop, *distance);
+
+            mapped_distances.insert((source_stop_id, destination_stop_id), stop_distance);
         }
 
-        DistanceMatrix {
-            entries: distance_matrix,
+        DistanceService {
+            distances: mapped_distances,
         }
     }
 
-    fn get_distances_from(&self, stop: &'a Stop) -> impl Iterator<Item = &&DistanceMatrixEntry> {
-        self.entries
+    pub fn get_distance(&self, from: &Stop, to: &Stop) -> Option<f64> {
+        Some(self.distances.get(&(from.get_id(), to.get_id()))?.get_distance())
+    }
+
+    fn get_distances_from(&self, stop: &'a Stop) -> impl Iterator<Item = &DistanceMatrixEntry> {
+        self.distances
             .iter()
             .filter(|x| x.0 .0 == stop.get_id())
             .map(|x| x.1)
@@ -39,5 +52,18 @@ impl<'a> DistanceMatrix<u32, &DistanceMatrixEntry<'a>> {
         self.get_distances_from(stop)
             .min_by(|stop1, stop2| stop1.partial_cmp(stop2).unwrap())
             .map(|x| x.get_destination_stop())
+    }
+
+    pub fn get_k_nearest_stops(&'a self, stop: &'a Stop, k: usize) -> Vec<&Stop> {
+        let mut stops = self
+            .get_distances_from(stop)
+            .collect::<Vec<&DistanceMatrixEntry>>();
+
+        stops.sort_by(|stop1, stop2| stop1.partial_cmp(stop2).unwrap());
+
+        stops[0..k]
+            .iter()
+            .map(|x| x.get_destination_stop())
+            .collect()
     }
 }
