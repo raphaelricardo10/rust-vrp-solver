062d4e639c135f4abf35186dd42fa5391f9c83c2
Raphael <raphaelricardo10@gmail.com>
2022-11-07 00:46:02 -0300

    Formatted code

--- src/tests/route_test.rs
+++ src/tests/route_test.rs
@@ -15,9 +15,30 @@ fn route_distance_calculation(stops: Vec<Stop>, distances: DistanceMatrix) {
     let mut route = Route::new(&mut vehicle);
 
     route.add_stop(&stops[0], 0.0).unwrap();
-    route.add_stop(&stops[1], *distances.get(&(stops[0].get_id(), stops[1].get_id())).unwrap()).unwrap();
-    route.add_stop(&stops[2], *distances.get(&(stops[1].get_id(), stops[2].get_id())).unwrap()).unwrap();
-    route.add_stop(&stops[3], *distances.get(&(stops[2].get_id(), stops[3].get_id())).unwrap()).unwrap();
+    route
+        .add_stop(
+            &stops[1],
+            *distances
+                .get(&(stops[0].get_id(), stops[1].get_id()))
+                .unwrap(),
+        )
+        .unwrap();
+    route
+        .add_stop(
+            &stops[2],
+            *distances
+                .get(&(stops[1].get_id(), stops[2].get_id()))
+                .unwrap(),
+        )
+        .unwrap();
+    route
+        .add_stop(
+            &stops[3],
+            *distances
+                .get(&(stops[2].get_id(), stops[3].get_id()))
+                .unwrap(),
+        )
+        .unwrap();
 
     assert_eq!(route.total_distance(), 9.0);
 }
@@ -29,7 +50,9 @@ fn route_cannot_overload_vehicle(full_stops: Vec<Stop>, distances: DistanceMatri
     let mut route = Route::new(&mut vehicle);
 
     route.add_stop(&full_stops[0], 0.0).unwrap();
-    let distance = *distances.get(&(full_stops[0].get_id(), full_stops[1].get_id())).unwrap();
+    let distance = *distances
+        .get(&(full_stops[0].get_id(), full_stops[1].get_id()))
+        .unwrap();
 
     match route.add_stop(&full_stops[1], distance) {
         Ok(_) => assert!(false),
