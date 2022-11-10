18960df015d05a833be99b8ff929bb497e005989
Raphael <raphaelricardo10@gmail.com>
2022-11-10 20:24:14 -0300

    Refactor: added some functions to make the logic cleaner

--- src/local_search/two_opt.rs
+++ src/local_search/two_opt.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 use crate::{
     domain::{route::Route, stop::Stop},
     services::distance::distance_service::DistanceService,
@@ -32,18 +34,18 @@ pub(crate) fn calculate_swap_cost<'a>(
 pub(crate) fn get_minimum_swap_cost<'a>(
     stops: &'a Vec<Stop>,
     distance_service: &'a DistanceService,
-    current_path: &'a Path<'a>,
+    path: &'a Path<'a>,
 ) -> Option<(usize, f64)> {
     stops
         .windows(3)
         .enumerate()
-        .skip(current_path.get_current().get_index() + 1)
-        .map(|(next_stop_index, next_stop_window)| {
+        .skip(path.get_next().get_index())
+        .map(|(base_index, next_stop_window)| {
             (
-                next_stop_index,
+                base_index + 1,
                 calculate_swap_cost(
-                    current_path,
-                    &Path::from_window(next_stop_window, next_stop_index, distance_service)
+                    path,
+                    &Path::from_window(next_stop_window, base_index, distance_service)
                         .unwrap(),
                     distance_service,
                 ),
@@ -52,23 +54,52 @@ pub(crate) fn get_minimum_swap_cost<'a>(
         .min_by(|(_, cost1), (_, cost2)| cost1.partial_cmp(cost2).unwrap())
 }
 
-fn should_swap_stops(path1: &Path, path2: &Path, swap_cost: &f64) -> bool{
+fn should_swap_stops(path1: &Path, path2: &Path, swap_cost: &f64) -> bool {
     *swap_cost < path1.get_cost() + path2.get_cost()
 }
 
+fn find_improvements(
+    stops: &Vec<Stop>,
+    distance_service: &DistanceService,
+    path: &Path,
+) -> Option<usize> {
+    let (swap_candidate_index, swap_cost) = get_minimum_swap_cost(stops, distance_service, &path)?;
+
+    let candidate_path = Path::from_stop_index(stops, swap_candidate_index, distance_service)?;
+
+    if should_swap_stops(&path, &candidate_path, &swap_cost) {
+        Some(candidate_path.get_current().get_index());
+    }
+
+    None
+}
+
+fn map_paths<'a>(
+    stops: &'a Vec<Stop>,
+    distance_service: &'a DistanceService,
+) -> BTreeMap<usize, Path<'a>> {
+    stops
+        .windows(3)
+        .enumerate()
+        .map(|(base_index, window)| {
+            (
+                base_index,
+                Path::from_window(window, base_index, distance_service).unwrap(),
+            )
+        })
+        .collect()
+}
+
 pub fn search(route: &mut Route, distance_service: &DistanceService) -> Option<bool> {
-    for (base_index, window) in route.get_stops().windows(3).enumerate() {
-        let path1 =
-            Path::from_window(window, base_index, distance_service)?;
+    let mut paths = map_paths(route.get_stops(), distance_service);
 
-        let (swap_candidate_index, swap_cost) =
-            get_minimum_swap_cost(route.get_stops(), distance_service, &path1)?;
+    for path in paths.values() {
+        let candidate_index = match find_improvements(route.get_stops(), distance_service, path) {
+            Some(candidate_index) => candidate_index,
+            None => continue,
+        };
 
-        let path2 = Path::from_stop_index(route.get_stops(), swap_candidate_index, distance_service)?;
-
-        if should_swap_stops(&path1, &path2, &swap_cost){
-            return Some(true);
-        }
+        //route.swap_stops(base_index, candidate_index)
     }
 
     return Some(false);
