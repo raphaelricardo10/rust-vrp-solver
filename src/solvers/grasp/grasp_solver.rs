809b94f9874895fc33c661767af2848b190b2e55
Raphael <raphaelricardo10@gmail.com>
2022-11-15 12:05:57 -0300

    Refactor: reordered method for better readability

--- src/solvers/grasp/grasp_solver.rs
+++ src/solvers/grasp/grasp_solver.rs
@@ -35,52 +35,14 @@ impl<'a> GraspSolver {
         }
     }
 
-    pub fn get_random_near_stop(&self, vehicle_id: u32) -> Option<&Stop> {
-        let near_stops = self
-            .route_service
-            .get_k_nearest_stops(vehicle_id, self.rcl_size)?;
-        let chosen = *near_stops.choose(&mut thread_rng())?;
-
-        Some(chosen)
-    }
-
-    fn run_iteration(&mut self, vehicle_ids: &Vec<u32>) {
-        for vehicle_id in vehicle_ids {
-            let stop_id = match self.get_random_near_stop(*vehicle_id) {
-                None => break,
-                Some(stop) => stop.id,
-            };
-
-            self.route_service
-                .assign_stop_to_route(*vehicle_id, stop_id)
-                .unwrap();
-
-            let route = self.route_service.get_route_mut(*vehicle_id).unwrap();
-            self.local_search.run(route);
+    pub fn solve(&mut self) {
+        while !self.stop_condition_met() {
+            self.run_generation();
         }
     }
 
-    fn generate_solution(&mut self, vehicle_ids: &Vec<u32>) {
-        self.route_service.reset();
-        self.route_service.assign_starting_points();
-
-        while self.route_service.has_available_stop().unwrap() {
-            self.run_iteration(vehicle_ids)
-        }
-
-        self.route_service.assign_stop_points();
-    }
-
-    fn run_local_search(&mut self, vehicle_ids: &Vec<u32>) {
-        for vehicle_id in vehicle_ids {
-            let route = self.route_service.get_route_mut(*vehicle_id).unwrap();
-
-            self.local_search.run(route);
-        }
-    }
-
-    fn should_update_solution(&self, solution: &Solution) -> bool {
-        solution.is_better_than(&self.solution)
+    fn stop_condition_met(&self) -> bool {
+        self.times_without_improvement >= self.max_improvement_times
     }
 
     fn run_generation(&mut self) {
@@ -107,13 +69,52 @@ impl<'a> GraspSolver {
         }
     }
 
-    fn stop_condition_met(&self) -> bool {
-        self.times_without_improvement >= self.max_improvement_times
+    fn generate_solution(&mut self, vehicle_ids: &Vec<u32>) {
+        self.route_service.reset();
+        self.route_service.assign_starting_points();
+
+        while self.route_service.has_available_stop().unwrap() {
+            self.run_iteration(vehicle_ids)
+        }
+
+        self.route_service.assign_stop_points();
     }
 
-    pub fn solve(&mut self) {
-        while !self.stop_condition_met() {
-            self.run_generation();
+    fn run_iteration(&mut self, vehicle_ids: &Vec<u32>) {
+        for vehicle_id in vehicle_ids {
+            let stop_id = match self.get_random_near_stop(*vehicle_id) {
+                None => break,
+                Some(stop) => stop.id,
+            };
+
+            self.route_service
+                .assign_stop_to_route(*vehicle_id, stop_id)
+                .unwrap();
+
+            let route = self.route_service.get_route_mut(*vehicle_id).unwrap();
+            self.local_search.run(route);
         }
     }
+
+    pub fn get_random_near_stop(&self, vehicle_id: u32) -> Option<&Stop> {
+        let near_stops = self
+            .route_service
+            .get_k_nearest_stops(vehicle_id, self.rcl_size)?;
+        let chosen = *near_stops.choose(&mut thread_rng())?;
+
+        Some(chosen)
+    }
+
+    fn run_local_search(&mut self, vehicle_ids: &Vec<u32>) {
+        for vehicle_id in vehicle_ids {
+            let route = self.route_service.get_route_mut(*vehicle_id).unwrap();
+
+            self.local_search.run(route);
+        }
+    }
+
+    fn should_update_solution(&self, solution: &Solution) -> bool {
+        solution.is_better_than(&self.solution)
+    }
+
 }
