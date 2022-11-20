c258004acbc9eff693dd296078f360b89a3081e1
Raphael Goncalves <raphaelricardo10@gmail.com>
2022-11-20 03:00:09 +0000

    Feat: added methods to get the k bests individuals and sort them

--- src/solvers/genetic/population.rs
+++ src/solvers/genetic/population.rs
@@ -1,5 +1,3 @@
-use crate::services::route::route_service::RouteService;
-
 use super::individual::Individual;
 
 #[derive(Default)]
@@ -9,6 +7,17 @@ pub(crate) struct Population {
 
 impl Population {
     pub(super) fn new(individuals: Vec<Individual>) -> Self {
+
         Self { individuals }
+    }
+
+    pub(super) fn sort_individuals(&mut self) {
+        self.individuals.sort_by(|individual1, individual2| {
+            individual1.fitness.partial_cmp(&individual2.fitness).unwrap()
+        });
+    }
+
+    pub(super) fn get_k_bests(&self, k: usize) -> &[Individual] {
+        &self.individuals[..k]
     }
 }
