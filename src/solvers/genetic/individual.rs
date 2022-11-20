044ffbc1731e64240771b4d96da65dd3913d7ced
Raphael Goncalves <raphaelricardo10@gmail.com>
2022-11-20 02:16:10 +0000

    feat: added methods to get, insert and swap genes

--- src/solvers/genetic/individual.rs
+++ src/solvers/genetic/individual.rs
@@ -2,6 +2,7 @@ use crate::domain::{route::Route, stop::Stop};
 
 type Gene = Stop;
 type Chromosome = Route;
+type GeneAddress = (usize, usize);
 
 pub(crate) struct Individual {
     pub(crate) fitness: f64,
@@ -23,5 +24,27 @@ impl Individual {
             .iter()
             .map(|chromosome| chromosome.total_distance())
             .sum()
+    }
+
+    pub(crate) fn get_gene(&self, address: GeneAddress) -> Option<Gene> {
+        self.chromosomes.get(address.0)?.stops.get(address.1).copied()
+    }
+
+    pub(crate) fn insert_gene(&mut self, address: GeneAddress, gene: Gene) -> Option<()> {
+        self.chromosomes.get_mut(address.0)?.stops[address.1] = gene;
+        Some(())
+    }
+
+    pub(crate) fn swap_genes(&mut self, address1: GeneAddress, address2: GeneAddress, fitness_change: f64) -> Option<()> {
+        let gene1 = self.get_gene(address1)?;
+        let gene2 = self.get_gene(address2)?;
+        
+        let aux = gene1;
+        self.insert_gene(address1, gene2);
+        self.insert_gene(address2, aux);
+
+        self.fitness += fitness_change;
+
+        Some(())
     }
 }
