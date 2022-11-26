64c9f7dfdf2efa06d7a56b7aa3cc5f96ead15e47
Raphael Goncalves <raphaelricardo10@gmail.com>
2022-11-26 15:59:11 +0000

    🧪 test: Consecutive paths
    tested if it can calculate the swap cost of consecutive paths in any order

--- src/tests/stop_swapper.rs
+++ src/tests/stop_swapper.rs
@@ -24,3 +24,18 @@ fn can_get_the_minimum_swap_cost(stop_swapper: StopSwapper, path_factory: PathFa
 
     assert_eq!(swap_cost.1, -5.0);
 }
+
+#[rstest]
+fn can_calculate_path_swap_cost_of_consecutive_swaps(
+    stop_swapper: StopSwapper,
+    path_factory: PathFactory,
+) {
+    let path1 = path_factory.make_path(1).unwrap();
+    let path2 = path_factory.make_path(2).unwrap();
+
+    let swap_cost1 = stop_swapper.calculate_swap_cost(&path1, &path2);
+    let swap_cost2 = stop_swapper.calculate_swap_cost(&path2, &path1);
+
+    assert_eq!(swap_cost1, -4.5);
+    assert_eq!(swap_cost2, -4.5);
+}
