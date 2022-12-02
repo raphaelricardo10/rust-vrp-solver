9ebfb03437740be92fc15feedf27e35fad4da493
Raphael Goncalves <raphaelricardo10@gmail.com>
2022-12-02 19:53:20 +0000

    🌈 style: format code

--- src/tests/fixtures/mod.rs
+++ src/tests/fixtures/mod.rs
@@ -1,9 +1,9 @@
 pub mod distances_fixture;
+pub mod genetic_algorithm_fixture;
+pub mod greedy_solver_fixture;
+pub mod routes_fixture;
+pub mod services_fixture;
 pub mod stop_swapper_fixture;
 pub mod stops_fixture;
 pub mod two_opt;
 pub mod vehicles_fixture;
-pub mod routes_fixture;
-pub mod services_fixture;
-pub mod genetic_algorithm_fixture;
-pub mod greedy_solver_fixture;
