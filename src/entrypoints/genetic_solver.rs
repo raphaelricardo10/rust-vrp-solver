11e8a985315c2309dc3448fc38daa2624328cba8
Raphael Goncalves <raphaelricardo10@gmail.com>
2022-12-05 03:19:16 +0000

    ✨ feat: genetic solver lib
    implement a basic lib to use the genetic algorithm solver

--- src/entrypoints/genetic_solver.rs
+++ src/entrypoints/genetic_solver.rs
@@ -2,42 +2,53 @@ use rand::thread_rng;
 
 use crate::{
     domain::{stop::Stop, vehicle::Vehicle},
-    services::{distance::distance_service::DistanceMatrix, route::route_service::RouteService},
+    services::route::route_service::RouteService,
     solvers::genetic::genetic_solver::GeneticSolver,
 };
 
-use super::parameters::GeneticAlgorithmParameters;
+use super::{
+    c_interfaces::c_distance_matrix::CDistanceMatrixEntry,
+    factories::{copy_result, distance_matrix_factory, vector_factory},
+    parameters::GeneticAlgorithmParameters,
+};
 
+/// # Safety
+///
+/// Make sure that all the size are consistent.
 #[no_mangle]
-pub extern "C" fn genetic_solver(
+pub unsafe extern "C" fn genetic_solver(
     vehicles_ptr: *mut Vehicle,
     stops_ptr: *mut Stop,
     parameters: GeneticAlgorithmParameters,
-    distances_ptr: *mut f64,
+    distances_ptr: *mut CDistanceMatrixEntry,
+    len_distances: usize,
     result: *mut u32,
 ) {
     let mut rng = thread_rng();
 
-    todo!();
+    let vehicles = unsafe { vector_factory(vehicles_ptr, parameters.number_of_routes) };
+    let stops = unsafe { vector_factory(stops_ptr, parameters.number_of_stops) };
 
-    // let vehicles = make_vector(vehicles_ptr, parameters.number_of_routes);
-    // let stops = make_vector(stops_ptr, parameters.number_of_stops);
+    let distances = unsafe { distance_matrix_factory(distances_ptr, len_distances) };
 
-    // let distances = &make_distances();
+    let route_service = RouteService::new(vehicles, &distances, stops.clone());
 
-    // let route_service = RouteService::new(vehicles, distances, stops.clone());
+    let mut genetic_solver = GeneticSolver::new(
+        stops,
+        &distances,
+        parameters.population_size,
+        parameters.elite_size,
+        parameters.mutation_rate,
+        parameters.max_crossover_tries,
+        parameters.max_generations,
+        route_service,
+        &mut rng,
+    );
 
-    // let mut genetic_solver = GeneticSolver::new(
-    //     stops,
-    //     distances,
-    //     parameters.population_size,
-    //     parameters.elite_size,
-    //     parameters.mutation_rate,
-    //     parameters.max_crossover_tries,
-    //     parameters.max_generations,
-    //     route_service,
-    //     &mut rng,
-    // );
+    genetic_solver.solve();
 
-    // genetic_solver.solve();
+    copy_result(
+        genetic_solver.solution.result.get(&0).unwrap().to_vec(),
+        result,
+    );
 }
