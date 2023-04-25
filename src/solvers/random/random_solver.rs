2d2511570ab4a11f483457c0a7218f5bd8fd03c0
Raphael Gonçalves <89359384+raphaelricardo10@users.noreply.github.com>
2023-04-25 14:25:00 +0000

    🐞 fix: solution reset
    missing the reset of route service to make all the routes available again

--- src/solvers/random/random_solver.rs
+++ src/solvers/random/random_solver.rs
@@ -50,6 +50,7 @@ impl<'a, R: Rng + ?Sized> Solver for RandomSolver<'a, R> {
     }
 
     fn reset_solution(&mut self) {
+        self.route_service.reset();
         self.solution = Default::default();
     }
 
