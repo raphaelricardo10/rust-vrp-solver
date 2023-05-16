add1f13f56ad2577907a33989d8410262036d8fd
Raphael Gonçalves <89359384+raphaelricardo10@users.noreply.github.com>
2023-05-16 14:53:02 +0000

    🦄 refactor: rm struct fields
    removed fields that are not used and any other methods and used local variables inside method instead.

--- src/solvers/grasp/grasp_solver.rs
+++ src/solvers/grasp/grasp_solver.rs
@@ -10,17 +10,15 @@ pub trait GraspSolverCallbacks {
 
 pub trait SolverWithGraspCallbacks<S: Solution>: Solver<S> + GraspSolverCallbacks {}
 
-pub struct GraspSolver<S: Solution + Default + Clone> {
+pub struct GraspSolver<S: Solution + Default> {
     max_improvement_times: u8,
-    times_without_improvement: u8,
-    best_solution: S,
     first_stage_solver: Box<dyn SolverWithGraspCallbacks<S>>,
     local_searcher: Box<dyn LocalSearcher<S> + 'static>,
 }
 
 impl<S> GraspSolver<S>
 where
-    S: Solution + Default + Clone,
+    S: Solution + Default,
 {
     pub fn new(
         max_improvement_times: u8,
@@ -31,37 +29,34 @@ where
             local_searcher,
             max_improvement_times,
             first_stage_solver,
-            best_solution: Default::default(),
-            times_without_improvement: Default::default(),
         }
-    }
-
-    fn stop_condition_met(&self) -> bool {
-        self.times_without_improvement >= self.max_improvement_times
     }
 }
 
 impl<S> Solver<S> for GraspSolver<S>
 where
-    S: Solution + Default + Clone,
+    S: Solution + Default,
 {
     fn solve(&mut self) -> S {
-        while !self.stop_condition_met() {
+        let mut best_solution = Default::default();
+        let mut times_without_improvement: u8 = 0;
+
+        while times_without_improvement < self.max_improvement_times {
             self.first_stage_solver.before_iteration();
 
             let mut solution = self.first_stage_solver.solve();
             self.local_searcher.run(&mut solution);
 
-            if solution.is_better_than(&self.best_solution) {
-                self.best_solution = solution;
-                self.times_without_improvement = 0;
+            if solution.is_better_than(&best_solution) {
+                best_solution = solution;
+                times_without_improvement = 0;
             } else {
-                self.times_without_improvement += 1;
+                times_without_improvement += 1;
             }
 
             self.first_stage_solver.after_iteration();
         }
 
-        self.best_solution.clone()
+        best_solution
     }
 }
